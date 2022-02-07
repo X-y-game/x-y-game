80ea1cf533d785e55e993830768b4d2ed49e28f2
Luceta <57941164+Luceta@users.noreply.github.com>
2022-02-07 17:56:52 +0900

    ➕Add: styled-reset 💄Add: Global styled component

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import GlobalStyle from "../../styles/GlobalStyle";
 import Home from "../Home";
 
 export default function App() {
   return (
     <Switch>
+      <GlobalStyle />
       <Route exact path="/">
         <Home />
       </Route>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import App from "./components/app/index.js";
+import App from "./components/app/index";
 
 ReactDOM.render(
   <React.StrictMode>
