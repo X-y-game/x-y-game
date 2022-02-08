761584e9b61af1352b1d58504fe34486fc93429e
Luceta <57941164+Luceta@users.noreply.github.com>
2022-02-08 19:46:00 +0900

    💄Update: Global style table 💄Add: Result Component UI ➕Add: React Table

---
+++ src/components/Table/index.js
@@ -0,0 +1,40 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from "react";
+import { useTable } from "react-table";
+import PropTypes from "prop-types";
+
+export default function Table({ columns, data }) {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
+
+  return (
+    <table {...getTableProps()}>
+      <caption>최종 결과</caption>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => (
+                <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+Table.propTypes = {
+  columns: PropTypes.arrayOf(PropTypes.object).isRequired,
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
