e54e1bfea5871bf7725fedc1fb78e5ae73e0bc70
whara123 <whara123@gmail.com>
2022-02-10 06:09:18 +0900

    💄 Modify room style

--- src/pages/Room/index.js
+++ src/pages/Room/index.js
@@ -1,60 +1,57 @@
 import React from "react";
 import styled from "styled-components";
-import { FaChevronLeft } from "react-icons/fa";
 import SetList from "./setList";
 
-export default function Channel() {
+export default function Room() {
   const handleClick = () => {
     console.log("뒤로");
   };
 
   return (
-    <ChannelBody>
+    <Body>
       <ChannelPage>
         <Header>
           <button type="button" onClick={handleClick} className="backBtn">
-            <FaChevronLeft />
+            {"<"}
           </button>
           <h2>룸을 선택하세요</h2>
         </Header>
         <SetList />
       </ChannelPage>
-    </ChannelBody>
+    </Body>
   );
 }
 
-const ChannelBody = styled.div`
-  background-color: #eee;
+const Body = styled.div`
+  background-color: #e0dede;
 `;
 
 const Header = styled.div`
   display: flex;
+  position: relative;
   width: 100%;
   justify-content: center;
   padding-bottom: 30px;
-  margin-bottom: 50px;
+  margin-bottom: 30px;
   border-bottom: 1px solid #000;
-  position: relative;
   h2 {
     display: inline-block;
-    font-size: 42px;
+    font-size: 2em;
   }
   .backBtn {
     position: absolute;
-    font-size: 25px;
-    background-color: inherit;
     left: 0;
     top: 50%;
-    margin-top: -35px;
     padding: 10px;
+    margin-top: -35px;
+    background-color: inherit;
+    font-size: 25px;
   }
 `;
 
 const ChannelPage = styled.div`
-  width: 50%;
-  height: 100vh;
   margin: 0 auto;
   padding: 20px;
   border-radius: 5px;
-  background-color: #fff;
+  background-color: #e0dede;
 `;
