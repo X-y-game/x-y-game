5c5a1e76b330a39025f4d7569ef7998fd19a8649
Luceta <57941164+Luceta@users.noreply.github.com>
2022-02-09 16:47:32 +0900

    💄Update: main text summary style

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -3,12 +3,14 @@ import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Home from "../../pages/Home";
 import Waiting from "../../pages/Waiting";
 import InGame from "../../pages/Ingame";
+import Channel from "../../pages/Channel";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/lobby" component={Channel} />
         <Route exact path="/waiting" component={Waiting} />
         <Route exact path="/game" component={InGame} />
       </Switch>
