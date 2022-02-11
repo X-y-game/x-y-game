7c51eae95c5834ebf5b84d4eb9e805ea536c749c
whara123 <64121533+whara123@users.noreply.github.com>
2022-02-11 15:01:16 +0900

    ♻️ Refactoring channel page

--- src/pages/Channel/List.js
+++ src/pages/Channel/List.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import PropTypes from "prop-types";
 
 export default function List({ text, id }) {
-  return <li id={id}>{text}</li>;
+  const history = useHistory();
+  const handelClick = () => {
+    history.push(`/lobby/${id}`);
+  };
+  return (
+    <li id={id} onClick={handelClick} onKeyDown={handelClick} aria-hidden="true">
+      {text}
+    </li>
+  );
 }
 
 List.propTypes = {
--- src/pages/Channel/index.js
+++ src/pages/Channel/index.js
@@ -3,22 +3,18 @@ import styled from "styled-components";
 import List from "./List";
 
 export default function Channel() {
+  const ChannelArr = Array(10)
+    .fill(1)
+    .map((v, i) => v + i);
   return (
     <ChannelBody>
       <ChannelPage>
         <Title>채널을 선택하세요</Title>
-        <WrapChannelUL>
-          <List id="1" text="1 채널" />
-          <List id="2" text="2 채널" />
-          <List id="3" text="3 채널" />
-          <List id="4" text="4 채널" />
-          <List id="5" text="5 채널" />
-          <List id="6" text="6 채널" />
-          <List id="7" text="7 채널" />
-          <List id="8" text="8 채널" />
-          <List id="9" text="9 채널" />
-          <List id="10" text="10 채널" />
-        </WrapChannelUL>
+        <WrapChannel>
+          {ChannelArr.map((it) => (
+            <List key={it} id={it} text={`${it}채널`} />
+          ))}
+        </WrapChannel>
       </ChannelPage>
     </ChannelBody>
   );
@@ -35,27 +31,27 @@ const ChannelPage = styled.div`
 `;
 
 const Title = styled.h2`
-  font-size: 2em;
-  text-align: center;
-  padding-bottom: 20px;
   margin-bottom: 30px;
+  padding-bottom: 20px;
   border-bottom: 1px solid #000;
+  text-align: center;
+  font-size: 2em;
 `;
 
-const WrapChannelUL = styled.ul`
+const WrapChannel = styled.ul`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   gap: 20px;
-  font-size: 24px;
+  font-size: 1.5em;
   li {
     width: 100%;
     padding: 20px;
     background-color: #fbf2f2;
-    cursor: pointer;
-    transition: 0.2s;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    transition: 0.2s;
+    cursor: pointer;
   }
   li:hover {
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
