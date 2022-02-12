0d85b113b2e294f7a06453f8bc947e5d94149c03
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-02-13 01:19:22 +0900

    ✨  Add : Choose team and check ready with socket

--- src/pages/Waiting/index.js
+++ src/pages/Waiting/index.js
@@ -1,31 +1,77 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-
+import { useHistory } from "react-router-dom";
+import socket from "../../components/utils/socket";
 import Team from "../../components/Team";
 
 export default function WaitingRoom() {
   const [isReady, setIsReady] = useState(false);
   const [team, setTeam] = useState(0);
+  const [canStart, setCanStart] = useState(false);
+
+  const channelId = 1;
+  const roomId = 1;
+  const roomName = `${channelId}-${roomId}-${team}`;
+
+  const history = useHistory();
+  const handleOnClickStart = () => {
+    history.push(`/game/:${roomName}-team${team}`);
+  };
+
+  useEffect(() => {
+    socket.on("connect", () => {});
+    socket.emit("join", roomName);
+  }, []);
+
+  useEffect(() => {
+    socket.emit("check_teams_ready", roomName);
+    socket.on("can_start", (c) => {
+      setCanStart(c);
+    });
+  }, [isReady, canStart]);
 
   const handleReady = () => {
-    setIsReady(!isReady);
-    if (!isReady) {
-      const myTeam = team;
+    if (window.confirm(`${team}팀으로 결정하시겠습니까?`)) {
+      setIsReady(!isReady);
+      if (!isReady) {
+        socket.emit("select_team", channelId, roomId, roomName, team);
+      }
     }
   };
 
   return (
     <Waiting>
       <Header>팀 선택</Header>
-      <Teams htmlFor="team">
+      <Teams htmlFor="team" style={{ display: isReady ? "none" : "grid" }}>
         {[1, 2, 3, 4].map((it) => (
-          <Team id={it} key={`team_${it}`} setTeam={setTeam} setIsReady={setIsReady} />
+          <Team id={it} key={`team_${it}`} setTeam={setTeam} isReady={isReady} handleReady={handleReady} />
         ))}
       </Teams>
-      <Button style={{ backgroundColor: isReady ? "#f2aeae" : "#e0dede" }} onClick={handleReady}>
-        {isReady ? "선택완료" : "선택하기"}
+      <Button
+        style={{ backgroundColor: isReady ? "#f2aeae" : "#e0dede", display: isReady ? "none" : "inline" }}
+        onClick={handleReady}
+      >
+        선택하기
       </Button>
-      <p style={{ display: isReady ? "block" : "none" }}>{team}팀을 선택했습니다</p>
+      <div style={{ display: isReady ? "block" : "none", margin: "10px" }}>
+        <strong style={{ display: "block", fontSize: "18px", fontWeight: "700", margin: "20px" }}>
+          {team}팀을 선택했습니다
+        </strong>
+        {canStart ? (
+          <div>
+            모든 팀의 선택이 완료되었습니다
+            <br /> 게임을 시작해봅시다 <br />
+          </div>
+        ) : (
+          <div>
+            다른 팀의 접속을 기다리는 중입니다
+            <br /> 잠시만 기다려주세요 <br />
+          </div>
+        )}
+      </div>
+      <StartBtn style={{ display: isReady && canStart ? "inline-block" : "none" }} onClick={handleOnClickStart}>
+        게임 시작
+      </StartBtn>
     </Waiting>
   );
 }
@@ -51,6 +97,14 @@ const Teams = styled.label`
 const Button = styled.button`
   padding: 10px;
   margin-bottom: 30px;
+  border: 2px solid #f2aeae;
+  border-radius: 10px;
+  background-color: #e0dede;
+`;
+
+const StartBtn = styled.button`
+  padding: 10px;
+  margin: 20px;
   border: 2px solid #f2aeae;
   border-radius: 10px;
   background-color: #e0dede;
