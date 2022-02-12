28d6acc9f0ffb93eb2b7125f60f794ae6ae30849
Luceta <57941164+Luceta@users.noreply.github.com>
2022-02-12 19:07:47 +0900

    ADD: ROOM api

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,6 @@
 const BASE_URL = "http://localhost8000/";
 const CHANNELS = `${BASE_URL}/channel`;
+const MAKE_ROOM = `${BASE_URL}/room`;
 
 export const getChannelsAPI = () => {
   const options = {
@@ -21,4 +22,27 @@ export const makeChannelAPI = (title, password) => {
   };
 
   return fetch(CHANNELS, options);
+};
+
+export const makeRoomAPI = (channelId, title) => {
+  const data = {
+    title,
+    channelId,
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+  };
+
+  return fetch(MAKE_ROOM, options);
+};
+
+export const getRoomAPI = (channelId) => {
+  const GET_ROOM = `${MAKE_ROOM}/${channelId}`;
+  const options = {
+    method: "GET",
+  };
+
+  return fetch(GET_ROOM, options);
 };
