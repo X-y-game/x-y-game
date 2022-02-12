90c7ab4125ac97216c3671c5e73fa697bf3a8c1b
whara123 <whara123@gmail.com>
2022-02-13 02:30:23 +0900

    🎨 Add prop channel, room id

--- src/pages/Room/roomList.js
+++ src/pages/Room/roomList.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 
-export default function roomList({ text, id }) {
+export default function roomList({ text, id, channelId }) {
   const history = useHistory();
   const handleClick = () => {
-    history.push(`/waiting/${id}`);
+    history.push({
+      pathname: `/waiting/${id}`,
+      state: { channel: channelId },
+    });
   };
   return (
     <li id={id} onClick={handleClick} onKeyDown={handleClick} aria-hidden="true">
