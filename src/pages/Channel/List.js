1cd8ea208c86a23d003d0ff61bd835c11d2711dd
whara123 <whara123@gmail.com>
2022-02-13 18:51:51 +0900

    🎨 Improve channel props dbID

--- src/pages/Channel/List.js
+++ src/pages/Channel/List.js
@@ -2,12 +2,12 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import PropTypes from "prop-types";
 
-export default function List({ text, id, channelId }) {
+export default function List({ text, id, channelId, num }) {
   const history = useHistory();
   const handleClick = () => {
     history.push({
-      pathname: `/lobby/${id}`,
-      state: { channel: channelId },
+      pathname: `/lobby/${channelId}`,
+      state: { channel: num },
     });
   };
   return (
@@ -21,4 +21,5 @@ List.propTypes = {
   text: PropTypes.string.isRequired,
   id: PropTypes.number.isRequired,
   channelId: PropTypes.string.isRequired,
+  num: PropTypes.number.isRequired,
 };
