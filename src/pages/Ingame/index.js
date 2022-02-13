9f8cd87c93b57105b2140e2ab8b849fb8a4d358f
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-02-14 04:00:04 +0900

    ✨  Add : Ingame socket, need to be fixed and tested

--- src/pages/Ingame/index.js
+++ src/pages/Ingame/index.js
@@ -1,15 +1,81 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation, useHistory } from "react-router-dom";
 import styled from "styled-components";
 import GameItem from "../../components/GameItem";
 import RuleBook from "../../components/RuleBook";
 import Modal from "../../components/Modal";
+import socket from "../../components/utils/socket";
 
 export default function Game() {
+  const location = useLocation();
+  const teamInfo = location.pathname.split(":")[1].split("-");
+  const [channelId, roomId, team] = teamInfo.slice(0, 3);
+  const roomName = `${channelId}-${roomId}`;
+  let round = Number(teamInfo[3]);
+
   const [mycard, setMycard] = useState("");
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [roundSelect, setRoundSelect] = useState([]); // 라운드별 선택
+  const [roundScore, setRoundScore] = useState([]); // 라운드 점수
+  const [roundDone, setRoundDone] = useState(false); // 라운드 종료 상태
+  const history = useHistory();
 
   const [isRuleModal, setIsRuleModal] = useState(false);
   const [isBoardModal, setIsBoardModal] = useState(false);
+
+  useEffect(() => {
+    socket.emit("join", roomName);
+  }, []);
+
+  // 리렌더링 테스트
+  useEffect(() => {
+    console.log(roundSelect);
+  }, [roundSelect]);
+
+  // 리렌더링 테스트
+  useEffect(() => {
+    console.log(roundDone);
+    // 최종 라운드가 아닌 경우 테스트
+    if (round < 10) {
+      socket.emit("get_score", (roomName, "all", round));
+      // 리렌더링 필요. 테스트 필요
+      socket.on("show_score", (results) => {
+        console.log(results);
+      });
+    }
+  }, [roundDone]);
+
+  // 중간결과. 테스트 필요
+  useEffect(() => {
+    console.log(isBoardModal);
+    socket.emit("get_score", (roomName, team, round));
+    socket.on("show_score", (results) => {
+      console.log(results);
+    });
+  }, [isBoardModal]);
+
+  const handleNext = () => {
+    // 라운드 종료 확인
+    if (roundDone) {
+      // 최종 라운드
+      if (round === 10) {
+        socket.emit("get_score", (roomName, "all", round));
+        // 리렌더링 필요. 테스트 필요
+        socket.on("show_score", (results) => {
+          console.log(results);
+        });
+      }
+      // 아래에서 라운드가 넘어갈 때
+      // 선택된 카드(라디오버튼)가 초기화되어야함
+      setIsSubmitted(false);
+      setMycard("");
+      round += 1;
+      history.push(`/game/:${roomName}-${team}-${round}`);
+    } else {
+      // line 39-44가 실행되어야합니다
+      console.log(roundScore);
+    }
+  };
 
   const handleToggleRule = () => {
     setIsRuleModal(!isRuleModal);
@@ -26,10 +92,15 @@ export default function Game() {
     setMycard(e.target.htmlFor);
   };
 
-  const handleSubmit = (e) => {
+  // 카드 제출. 리렌더링 테스트 필요
+  const handleSubmit = () => {
     setIsSubmitted(true);
-    // DB에 저장될 변수
-    const myCard = mycard;
+    socket.emit("select_card", roomName, team, round, mycard);
+    socket.on("show_round_result", (results, roundResult) => {
+      setRoundSelect(results);
+      setRoundScore(roundResult);
+      setRoundDone(true);
+    });
   };
 
   return (
@@ -37,8 +108,8 @@ export default function Game() {
       {isRuleModal && <RuleBook handleClick={handleToggleRule} />}
       {isBoardModal && <Modal handleClick={handleCurrentBoard} />}
       <Header>
-        <li>Team 1</li>
-        <li>Round 1</li>
+        <li>{team}</li>
+        <li>Round {round}</li>
         <li>
           <span aria-hidden="true" onClick={handleCurrentBoard} onKeyDown={handleCurrentBoard}>
             📊
@@ -50,15 +121,15 @@ export default function Game() {
       </Header>
       <GameItems>
         {[1, 2, 3, 4].map((item) => (
-          <GameItem id={item} key={`team_${item}`} isSubmitted={isSubmitted} mycard={mycard} />
+          <GameItem id={item} team={team} key={`team_${item}`} isSubmitted={isSubmitted} mycard={mycard} />
         ))}
       </GameItems>
       <Cards>
-        <Card id="X" name="choice" hidden />
+        <Card id="X" name="choice" hidden defaultChecked={isSubmitted} />
         <CardLabel htmlFor="X" onClick={handleSelect}>
           X
         </CardLabel>
-        <Card id="Y" name="choice" hidden />
+        <Card id="Y" name="choice" hidden defaultChecked={isSubmitted} />
         <CardLabel htmlFor="Y" onClick={handleSelect}>
           Y
         </CardLabel>
@@ -66,9 +137,15 @@ export default function Game() {
       <Footer>
         <li>현재 점수 : 100</li>
         <li>
-          <button type="button" onClick={handleSubmit}>
-            {isSubmitted ? "제출완료" : "제출하기"}
-          </button>
+          {isSubmitted ? (
+            <button type="button" onClick={handleNext}>
+              {roundDone ? "다음 라운드로" : "결과보기"}
+            </button>
+          ) : (
+            <button type="button" onClick={handleSubmit}>
+              제출하기
+            </button>
+          )}
         </li>
       </Footer>
     </InGame>
