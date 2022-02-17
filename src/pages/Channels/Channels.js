4bf3a3ed50da7ac5b4027cc1baad91f7c6edd1d2
whara123 <whara123@gmail.com>
2022-02-18 04:42:33 +0900

    🎨 Add header component

--- src/pages/Channels/Channels.js
+++ src/pages/Channels/Channels.js
@@ -2,12 +2,13 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes, { array, object } from "prop-types";
 import Channel from "../../components/Channel";
+import Header from "../../components/Header";
 
 export default function Channels({ channels }) {
   return (
     <ChannelBody>
       <ChannelPage>
-        <Title>채널을 선택하세요.</Title>
+        <Header title="채널을 선택하세요" channel="" roomId="" />
         <WrapChannel>
           {channels?.channelLists?.map(({ _id, title, password }, index) => (
             <Channel key={`channel${_id}`} index={index + 1} text={title} channelId={_id} pw={password} />
@@ -30,14 +31,6 @@ const ChannelPage = styled.div`
   margin: 0 auto;
   padding: 20px;
   border-radius: 5px;
-`;
-
-const Title = styled.h2`
-  margin-bottom: 30px;
-  padding-bottom: 20px;
-  border-bottom: 1px solid #000;
-  text-align: center;
-  font-size: 2em;
 `;
 
 const WrapChannel = styled.ul`
