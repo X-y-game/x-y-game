2ff9ce47e39f32ffe0bee05c4104f24d01ea2e0f
whara123 <whara123@gmail.com>
2022-02-17 22:00:52 +0900

    💄 ♻️ Refactor modal Prop name, UI

--- src/components/Modal/teamResult.js
+++ src/components/Modal/teamResult.js
@@ -4,11 +4,11 @@ import PropTypes from "prop-types";
 
 export default function TeamResult({ team, cardXY, point }) {
   return (
-    <div className="wrapCard">
+    <WrapCard className="wrapCard">
       <p>{team}팀</p>
       <SelectCard name={cardXY}>{cardXY}</SelectCard>
       <p>{point > 0 ? `+${point}` : point}</p>
-    </div>
+    </WrapCard>
   );
 }
 
@@ -19,6 +19,30 @@ const SelectCard = styled.div`
   width: 50%;
   background-color: ${(props) => (props.name === "X" ? "#c3e8fb" : "#ffb7b7")};
   font-size: 2em;
+  @media screen and (min-width: 600px) {
+    width: 60%;
+    height: 50%;
+    font-size: 4em;
+  }
+`;
+
+const WrapCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  width: 40%;
+  height: 100%;
+  padding: 10px;
+  background-color: #c1d0fb;
+  @media screen and (min-width: 600px) {
+    width: 20%;
+    height: 70%;
+    p {
+      font-size: 40px;
+    }
+  }
 `;
 
 TeamResult.propTypes = {
